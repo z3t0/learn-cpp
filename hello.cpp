fb9e2288558cfb5bda01e9937510e427e684ae5c
Rafi Khan <zetoslab@gmail.com>
2017-05-24 16:54:18 -0600

    added name input

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 int main() {
+  std::string name;
 
-  std::cout << "Hello, World!" << std::endl;
+  // Get name
+  std::cout << "Please enter your name: ";
+  std::cin >> name;
+
+  // Say hellp
+  std::cout << "Hello " << name << "!"  << std::endl;
 
   return 0;
 }
